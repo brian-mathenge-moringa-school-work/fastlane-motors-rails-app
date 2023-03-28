80fbe0ddfa8307769af812246d5df6e091e2fdba
Brian <wamwangimathenge@gmail.com>
2023-03-28 14:21:14 +0300

    User show route added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+    # GET "/users/:id"
+    def show
+        user = User.find(params[:id])
+        render json: user.to_json(include: {favorites: {include: :car, except:[:user_id, :car_id, :created_at, :updated_at]}}, except: [:password_digest, :created_at, :updated_at])
+    end
+
+
+    private
+
+    def user_params
+        params.permit(:email, :name, :password, :password_confirmation, :phone)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :cars, only: [:index, :show]
   resources :favorites, only: [:create, :destroy]
+  resources :users, only: [:show]
 end
