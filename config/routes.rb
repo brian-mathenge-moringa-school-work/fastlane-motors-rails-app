1fffca11571a7522a64fc8889ea40a23ed8554a3
Brian <wamwangimathenge@gmail.com>
2023-03-28 14:46:17 +0300

    Installed dependencies

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :cars, only: [:index, :show]
   resources :favorites, only: [:create, :destroy]
   resources :users, only: [:show, :create]
+  resources :sessions, only: [:create, :destroy]
 end
