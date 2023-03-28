7d5c2751d529455847f082abf7980d5b1b395e9b
Brian <wamwangimathenge@gmail.com>
2023-03-28 14:15:14 +0300

    Favorite create and destroy routes created

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,24 @@
 class FavoritesController < ApplicationController
+    def create
+        print params
+        if Car.find(params[:favorite][:car_id]) && User.find(params[:favorite][:user_id])
+            new_favorite = Favorite.create(favorite_params)
+        else 
+            new_favorite = favorite_params
+        end
+        render json: new_favorite.to_json(include: :car, except:[:user_id, :car_id, :created_at, :updated_at])
+    end
+
+    def destroy
+        favorite = Favorite.find(params[:id])
+        favorite.destroy
+        render json: favorite
+    end
+
+    private
+
+    def favorite_params
+        params.require(:favorite).permit(:car_id, :user_id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :cars, only: [:index, :show]
+  resources :favorites, only: [:create, :destroy]
 end
