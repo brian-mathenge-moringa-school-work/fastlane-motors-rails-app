f5f8cf9b4b2e8c7aa04c0bc6a6c4e11c0d82a38a
Brian <wamwangimathenge@gmail.com>
2023-04-02 20:23:52 +0300

    Users component modified

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
 
+    # GET "/users"
+    def index
+        users = User.all
+        render json: users
+    end
+
     # GET "/users/:id"
     def show
         user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :cars, only: [:index, :show]
   resources :favorites, only: [:create, :destroy]
-  resources :users, only: [:show, :create]
+  resources :users, only: [:index, :show, :create]
   resources :sessions, only: [:create, :show, :destroy]
 end
